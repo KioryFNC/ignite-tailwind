a08fb1770ba4ce386c7c37da207dc215dc7b2e58
KioryFNC <yuri.barros.luz.familia@gmail.com>
2025-04-29 17:18:06 -0300

    preview img

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,11 +1,29 @@
 'use client'
 
-import { ComponentProps } from 'react'
+import { ChangeEvent, ComponentProps } from 'react'
 import { useFileInput } from './Root'
 
 export interface ControlProps extends ComponentProps<'input'> {}
 
 export function Control(props: ControlProps) {
-  const { id } = useFileInput()
-  return <input type="file" className="sr-only" id={id} {...props} />
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files?.length) {
+      return
+    }
+
+    const files = Array.from(event.target.files)
+
+    onFilesSelected(files)
+  }
+  return (
+    <input
+      type="file"
+      className="sr-only"
+      id={id}
+      onChange={handleFilesSelected}
+      {...props}
+    />
+  )
 }
